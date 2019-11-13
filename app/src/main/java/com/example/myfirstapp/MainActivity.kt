2c1088c103af419afae002bd6f0f4f9b7dee3a67
João Ramos <joaomframos.1@gmail.com>
2019-11-13 14:12:21 +0000

    Added first interaction between app and application library (https://medium.com/@anujguptawork/how-to-create-your-own-android-library-and-publish-it-750e0f7481bf)

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.helppiertestlibrary.HelppierAPI
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        HelppierAPI.s(this, "Hello")
+        // ToasterMessage.s(this, "Hello")
     }
 }
--- helppiertestlibrary/src/main/java/com/example/helppiertestlibrary/HelppierAPI.java
+++ helppiertestlibrary/src/main/java/com/example/helppiertestlibrary/HelppierAPI.java
@@ -1,11 +1,12 @@
 package com.example.helppiertestlibrary;
 
 import android.content.Context;
-import 	android.widget.Toast;
+import android.util.Log;
+import android.widget.Toast;
 
 public class HelppierAPI {
     public static void s(Context c,String message){
-
+    Log.i("MyActivty", "test log");
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
     }
