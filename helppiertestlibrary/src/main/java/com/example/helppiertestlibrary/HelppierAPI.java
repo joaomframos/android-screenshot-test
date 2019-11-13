e48c67a4c617f91a6f628716e890ab379e92cba6
João Ramos <joaomframos.1@gmail.com>
2019-11-13 08:55:27 +0000

    Initial commit

---
+++ helppiertestlibrary/src/main/java/com/example/helppiertestlibrary/HelppierAPI.java
@@ -0,0 +1,12 @@
+package com.example.helppiertestlibrary;
+
+import android.content.Context;
+import 	android.widget.Toast;
+
+public class HelppierAPI {
+    public static void s(Context c,String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
