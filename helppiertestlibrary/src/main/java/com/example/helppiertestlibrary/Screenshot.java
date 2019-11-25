aea521a8fa88caed9f2ce1b6f230db5f4a3d4c31
João Ramos <joaomframos.1@gmail.com>
2019-11-25 09:13:53 +0000

    Screenshot is reaching widget as base64

--- helppiertestlibrary/src/main/java/com/example/helppiertestlibrary/Screenshot.java
+++ helppiertestlibrary/src/main/java/com/example/helppiertestlibrary/Screenshot.java
@@ -6,20 +6,44 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Environment;
 import android.text.format.DateFormat;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Date;
+import java.util.Hashtable;
+import java.util.Map;
 
 import static androidx.core.app.ActivityCompat.startActivityForResult;
 
 public class Screenshot {
     private static final int PICK_FILE_REQUEST = 123;
 
+    private String toBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+
     public void takeScreenshot(Activity activity, View view, ImageView screnshotView) {
+        final Activity finalActivity = activity;
         Date now = new Date();
         DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
         Log.i("TakeScreenshot", "Taking screenshot");
@@ -33,18 +57,81 @@ public class Screenshot {
             // Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
             // v1.setDrawingCacheEnabled(false);
 
-            Bitmap bitmap = Bitmap.createBitmap(view.getWidth(),
+            final Bitmap bitmap = Bitmap.createBitmap(view.getWidth(),
                     view.getHeight(), Bitmap.Config.ARGB_8888);
+            StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+            "http://10.0.2.2:3000/widget/android",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Showing toast message of the response
+                        try {
+                            JSONObject jsonObject = new JSONObject(s);
+                            int responseCode = Integer.parseInt(jsonObject.getString("responseCode"));
+                            String response = jsonObject.getString("response");
+                            if (responseCode == 1) {
+                                Toast.makeText(finalActivity, response, Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(finalActivity, "Error: " + response, Toast.LENGTH_LONG).show();
+                            }
+                        } catch (Exception ex) {
+                            Toast.makeText(finalActivity, "Failed to upload.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                        //Showing toast
+                        try {
+                            JSONObject jsonObject = new JSONObject(volleyError.getMessage());
+                            int responseCode = Integer.parseInt(jsonObject.getString("responseCode"));
+                            String response = jsonObject.getString("response");
+                            if (responseCode == 1) {
+                                Toast.makeText(finalActivity, response, Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(finalActivity, "Error: " + response, Toast.LENGTH_LONG).show();
+                            }
+                        } catch (Exception ex) {
+                            Toast.makeText(finalActivity, "Failed to upload.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    //Converting Bitmap to String
+                    String image = toBase64(bitmap);
+
+                    //Getting Image Name
+
+                    //Creating parameters
+                    Map<String,String> params = new Hashtable<>();
+
+                    //Adding parameters
+                    params.put("base64", image);
+
+                    //returning parameters
+                    return params;
+                }
+            };
+
+            RequestQueue queue = Volley.newRequestQueue(activity);
+            //Adding request to the queue
+            queue.add(stringRequest);
+
             Canvas canvas = new Canvas(bitmap);
             view.draw(canvas);
 
             screnshotView.setImageBitmap(bitmap);
-
+/*
             File imageFile = new File(mPath);
             FileOutputStream outputStream = new FileOutputStream(imageFile);
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
             outputStream.flush();
-            outputStream.close();
+            outputStream.close();*/
 
 /*
             FileOutputStream outputStream = new FileOutputStream(imageFile);
