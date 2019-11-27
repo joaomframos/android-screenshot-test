64e87f16490d8b83fcc85269f0e307f12480f0c9
João Ramos <joaomframos.1@gmail.com>
2019-11-27 07:58:12 +0000

    Added some comments regarding what's happening on the code ; removed the helper  image that displayed the screenshot in the UI

--- helppiertestlibrary/src/main/java/com/example/helppiertestlibrary/HelppierApp.java
+++ helppiertestlibrary/src/main/java/com/example/helppiertestlibrary/HelppierApp.java
@@ -28,11 +28,16 @@ import java.util.Map;
 
 
 public class HelppierApp {
+    // helppier authentication key
     private String helppierKey;
+    // the activity that we will be rendering over
     private Activity activity;
+    // the current view in use
     private LinearLayout view;
+    // reference to our screen shot UI
     private View screenshotUI;
 
+    // event listener for the screenshot button
     private View.OnClickListener screenshotListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -46,6 +51,12 @@ public class HelppierApp {
         this.view = view;
     }
 
+    public void init() {
+        renderScreenshotUI();
+    }
+
+
+    // converts a bitmap with the screenshot to a base 64
     private String toBase64(Bitmap bitmap) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
@@ -53,6 +64,7 @@ public class HelppierApp {
         return Base64.encodeToString(byteArray, Base64.DEFAULT);
     }
 
+    // inflates client activity with our screnshot UI
     private void renderScreenshotUI() {
         LayoutInflater layoutInflater = activity.getLayoutInflater();
         screenshotUI = layoutInflater.inflate(R.layout.screenshot, null, false);
@@ -63,37 +75,29 @@ public class HelppierApp {
         screenshotC.setOnClickListener(screenshotListener);
     }
 
+    // removes the screenshot UI we appended to the clients view
     private void removeScreenshotUI() {
         view.removeView(screenshotUI);
     }
 
-    public void init() {
-        renderScreenshotUI();
-    }
-
-    private void renderScreenshot(Bitmap bitmap) {
-        ImageView screenshotView = activity.findViewById(R.id.screenShot);
-
-        Canvas canvas = new Canvas(bitmap);
-        view.draw(canvas);
-
-        screenshotView.setImageBitmap(bitmap);
-    }
-
-
+    // take the screenshot and upload it
     private void takeScreenshot() {
         final Activity finalActivity = activity;
 
         Log.i("TakeScreenshot", "Taking screenshot");
 
         try {
+            // remove our UI from the screenshot
             removeScreenshotUI();
+            // take the bitmap / screenshot
             final Bitmap bitmap = Bitmap.createBitmap(
                     view.getWidth(),
                     view.getHeight(),
                     Bitmap.Config.ARGB_8888
             );
+            // re-add our UI to enable more screenshots
             renderScreenshotUI();
+
             StringRequest stringRequest = new StringRequest(
                 Request.Method.POST,
             "http://10.0.2.2:3000/widget/android",
@@ -156,8 +160,6 @@ public class HelppierApp {
             RequestQueue queue = Volley.newRequestQueue(activity);
             //Adding request to the queue
             queue.add(stringRequest);
-
-            renderScreenshot(bitmap);
 
 
         } catch (Throwable e) {
